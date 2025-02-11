d88ee08a68b5e110527e4919cdb3e3de599c21fc
IsraelRAlencar <israelalencarribeiro@gmail.com>
2025-02-11 17:17:50 -0400

    new - organization w/ useEffect, useReducer, useContext and useRef

--- src/components/H1/index.jsx
+++
@@ -1,8 +0,0 @@
-import { GlobalContext } from "../../contexts/AppContext";
-import { useContext } from "react";
-
-export const H1 = ( ) => {
-  const theContext = useContext(GlobalContext);
-  const { contextState: {title, counter} } = theContext;
-  return <h1>{title} {counter}</h1>
-};
--- src/components/P/index.jsx
+++
@@ -1,8 +0,0 @@
-import { GlobalContext } from "../../contexts/AppContext";
-import { useContext } from "react";
-
-export const P = ( ) => {
-  const theContext = useContext(GlobalContext);
-  const {contextState: {body}, setState} = theContext;
-  return <p onClick={() => setState((s) => ({...s, counter: s.counter + 1}))} >{body}</p>
-};
