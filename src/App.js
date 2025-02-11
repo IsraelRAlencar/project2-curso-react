470452e83fb166693a01ef11643f5916371ba07a
IsraelRAlencar <israelalencarribeiro@gmail.com>
2025-02-11 16:19:35 -0400

    new - learning how to create my own hook

--- src/App.js
+++ src/App.js
@@ -1,68 +1,23 @@
-import { createContext, useContext, useReducer, useRef } from 'react';
-import './App.css';
-import P from 'prop-types';
+import { useEffect, useState } from "react";
 
-// actions.js
-export const actions = {
-  CHANGE_TITLE: 'CHANGE_TITLE'
+const useMyHook = (cb) => {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      cb();
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, [cb]);
 };
 
-// Data.js
-export const globalState = {
-  title: 'O título do contexto',
-  body: 'O body do contexto',
-  counter: 0
-};
-
-// Reducer.js
-export const reducer = (state, action) => {
-  switch (action.type){
-    case actions.CHANGE_TITLE:
-      console.log('Mudar título');
-      return {...state, title: action.payload};
-    default:
-      console.log('default');
-      return {...state};
-  }
-
-};
-
-// AppContext.jsx
-export const Context = createContext();
-export const AppContext = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, globalState);
-
-  const changeTitle = (payload) => {
-    dispatch({ type: actions.CHANGE_TITLE, payload });
-  };
-
-  return <Context.Provider value={{ state, changeTitle }}>{children}</Context.Provider>
-};
-
-AppContext.propTypes = {
-  children: P.node
-};
-
-export const H1 = () => {
-  const context = useContext(Context);
-  const inputRef = useRef();
-
-  return (
-    <>
-      <h1 onClick={() => context.changeTitle(inputRef.current.value)}>{context.state.title}</h1>
-      <input type="text" ref={inputRef}/>
-    </>
-  );
-};
-
-//App.jsx 
 function App() {
+  const [counter, setCounter] = useState(0);
+  useMyHook(() => setCounter((c) => c + 1));
+
   return (
-    <AppContext>
-      <div>
-        <H1 />
-      </div>
-    </AppContext>
+    <div>
+      <h1>Contador: {counter}</h1>
+    </div>
   );
 }
 
