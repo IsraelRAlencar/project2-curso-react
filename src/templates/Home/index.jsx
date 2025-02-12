796f1d8b53ac715be57ed6f1d6b702a56179fc76
IsraelRAlencar <israelalencarribeiro@gmail.com>
2025-02-12 19:22:20 -0400

    new - compound components w/ Context API

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,4 +1,4 @@
-import { Children, cloneElement, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 const s = {
   style: {
@@ -6,35 +6,42 @@ const s = {
   }
 }
 
+const TurnOnOffContext = createContext(); 
+
 const TurnOnOff = ({children}) => {
   const [isOn, setIsOn] = useState(false);
   const onTurn = () => setIsOn((s) => !s);
 
-  return Children.map(children, (child) => {
-    const newChild = cloneElement(child, {
-      isOn,
-      onTurn
-    });
-
-    return newChild;
-  });
+  return <TurnOnOffContext.Provider value={{isOn, onTurn}}>{children}</TurnOnOffContext.Provider>;
 };
 
 const P = ({children}) => <p {...s}>{children}</p>;
 
-const TurnedOn = ({ isOn, children }) => (isOn ? children : null);
-const TurnedOff = ({ isOn, children }) => (isOn ? null : children);
+const TurnedOn = ({ children }) => {
+  const {isOn} = useContext(TurnOnOffContext);
+  return isOn ? children : null;
+};
 
-const TurnButton = ({isOn, onTurn, ...props}) => {
+const TurnedOff = ({ children }) => {
+  const {isOn} = useContext(TurnOnOffContext);
+  return isOn ? null : children;
+};
+
+const TurnButton = ({ ...props}) => {
+  const {isOn, onTurn} = useContext(TurnOnOffContext);
+
   return <button onClick={onTurn} {...props}>Turn {isOn ? 'OFF' : 'ON'}</button>;
 }
 
 export const Home = () => {
   return (
     <TurnOnOff>
-      <TurnedOn><P>Coisas do ON</P></TurnedOn>
-      <TurnedOff><P>Coisas do OFF</P></TurnedOff>
-      <TurnButton {...s}/>
+      <div>
+        <p>Oi</p>
+        <TurnedOn><P>Coisas do ON</P></TurnedOn>
+        <TurnedOff><P>Coisas do OFF</P></TurnedOff>
+        <TurnButton {...s}/>
+      </div>
     </TurnOnOff>
   );
 };
