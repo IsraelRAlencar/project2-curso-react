033450035ba1bcd36b75c8c3ad62e26486e05370
IsraelRAlencar <israelalencarribeiro@gmail.com>
2025-02-12 10:27:22 -0400

    new - new custom hook

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,35 +1,75 @@
-import { useState } from "react";
-import { useFetch } from "./use-fetch";
+import { useCallback, useEffect, useState } from "react";
 
-export const Home = () => {
-  const [postId, setPostId] = useState('');
-  const [result, loading] = useFetch('https://jsonplaceholder.typicode.com/posts/' + postId, {
-    headers: {
-      abc: '1' + postId,
-    }
+const useAsync = (asyncFunction, shouldRun) => {
+  const [state, setState] = useState({
+    result: null,
+    error: null,
+    status: 'idle'
   });
 
-  if (loading) {
-    return <p>Loading...</p>
-  }
+  const run = useCallback(async () => {
+    await new Promise(r => setTimeout(r, 1000));
+    setState({
+      result: null,
+      error: null,
+      status: 'pending'
+    });
 
-  const handleClick = (id) => {
-    setPostId(id);
-  }
+    return asyncFunction().then(response => {
+      setState({
+        result: response,
+        error: null,
+        status: 'settled'
+      });
+    }).catch(error => {
+      setState({
+        result: null,
+        error: error,
+        status: 'error'
+      });
+    });
+  }, [asyncFunction]);
 
-  if (!loading && result) {
-    return <div>
-        {result?.length > 0 ? result.map(p => 
-          <div key={`post-${p.id}`} onClick={() => handleClick(p.id)}>
-            <p >{p.title}</p>
-          </div>
-        ) : (
-          <div onClick={() => handleClick('')}>
-            <p>{result.title}</p>
-          </div>
-        )}
-    </div>;
-  }
+  useEffect(() => {
+    if (shouldRun) {
+      run();
+    }
+  }, [run, shouldRun]);
 
-  return <h1>Oi</h1>
+  return [run, state.result, state.error, state.status];
+};
+
+const fetchData = async () => {
+  // throw new Error('Ocorreu um erro');
+
+  await new Promise(r => setTimeout(r, 1000));
+  const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const json = await data.json();
+
+  return json;
+};
+
+export const Home = () => {
+  const [posts, setPosts] = useState(null);
+  const [reFetchData, result, error, status] = useAsync(fetchData, true);
+
+  function handleClick() {
+    reFetchData();
+  };
+
+  if (status === 'idle') {
+    return <pre>Nada executando</pre>;
+  }
+  
+  if (status === 'pending') {
+    return <pre>Loading...</pre>;
+  }
+  
+  if (status === 'error') {
+    return <pre>{error.message}</pre>;
+  }
+  
+  if (status === 'settled') {
+    return <pre onClick={handleClick}>{JSON.stringify(result, null, 2)}</pre>;
+  }
 };
